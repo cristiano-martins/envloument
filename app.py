7342d91b2b3b10ee418841b2987975d5e2a4e76e
Cristiano Martins <cristianomartinscrs26@gmail.com>
2026-09-22 19:50:43 +0100

    Change app host to '0.0.0.0' for accessibility

--- app.py
+++ app.py
@@ -364,4 +364,4 @@ def admin():
 init_db()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=int(os.getenv('PORT', '5000')), debug=os.getenv('FLASK_DEBUG') == '1')
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000))), debug=os.getenv('FLASK_DEBUG') == '1')
